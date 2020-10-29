e14e9f58ae068063cc52d32580159be4fe77b23c
git-nandor <nandor@leadforward.hu>
2020-10-29 21:10:05 +0100

    Add axios to the project and establish connection  with API

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./styles.css";
+import axios from "axios";
+import axiosConfig from "./axiosConfig";
 
 export default function App() {
+  const [allUserData, setAllUserData] = useState("");
+
+  useEffect(() => {
+    const handleRequest = async () => {
+      try {
+        const response = await axios.get();
+
+        if (response.status === 200) {
+          setAllUserData(response.data);
+          console.log(response.data);
+        }
+      } catch (error) {
+        console.log("Error in handleRequest: ", error);
+      }
+    };
+
+    handleRequest();
+  }, []);
+
   return (
     <div className="App">
       <h1>React Todo Manager</h1>
+      <div>{JSON.stringify(allUserData)}</div>
     </div>
   );
 }
